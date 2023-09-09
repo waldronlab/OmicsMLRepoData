cedcad258de5136d66db6e0d2496c294ad8d7dfb
Sehyun Oh <shbrief@hotmail.com>
2023-09-08 22:51:22 -0400

    add bodysubsite in the data dictionary

--- curatedMetagenomicData/R/template_bodysite.R
+++ curatedMetagenomicData/R/template_bodysite.R
@@ -3,6 +3,9 @@
 
 bodysite_map <- read.csv(file.path(dir, "maps/cMD_bodysite_ontology.csv"))
 
+bodysite <- bodysite_map[which(bodysite_map$bodysite_values == "Yes"),]
+bodysubsite <- bodysite_map[which(bodysite_map$bodysite_values == "No"),]
+
 ## bodysite
 curated_bodysite <- data.frame(
     col.name = "bodysite",
@@ -11,8 +14,22 @@ curated_bodysite <- data.frame(
     requiredness = "optional",
     multiplevalues = FALSE,
     description = "Subject's bodysite the given sample was collected from",
-    allowedvalues = paste(unique(bodysite_map$curated_ontology_term), 
+    allowedvalues = paste(unique(bodysite$curated_ontology_term), 
                           collapse = "|"),
-    ontology = paste(unique(basename(bodysite_map$curated_ontology_term_id)), 
+    ontology = paste(unique(basename(bodysite$curated_ontology_term_id)), 
+                     collapse = "|")     
+)
+
+## body_subsite
+curated_bodysubsite <- data.frame(
+    col.name = "body_subsite",
+    col.class = "character",
+    uniqueness = "non-unique", 
+    requiredness = "optional",
+    multiplevalues = FALSE,
+    description = "Subject's body subsite the given sample was collected from. Should be more specific information than bodysite",
+    allowedvalues = paste(unique(bodysubsite$curated_ontology_term), 
+                          collapse = "|"),
+    ontology = paste(unique(basename(bodysubsite$curated_ontology_term_id)), 
                      collapse = "|")     
 )
