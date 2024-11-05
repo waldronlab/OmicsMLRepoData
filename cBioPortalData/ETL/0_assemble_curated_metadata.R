d7e7fde3dcacb4eb9cff48b0794aecda29e7e5f1
Sehyun Oh <shbrief@hotmail.com>
2024-11-04 19:09:15 -0500

    specify the package

--- cBioPortalData/ETL/0_assemble_curated_metadata.R
+++ cBioPortalData/ETL/0_assemble_curated_metadata.R
@@ -10,7 +10,7 @@ curated <- list.files(dataDir)
 curated <- curated[grep(".csv", curated)]
 curatedDatObj <- gsub(".csv", "", curated)
 for (i in seq_along(curated)) {
-    res <- read_csv(file.path(dataDir, curated[i]))
+    res <- readr::read_csv(file.path(dataDir, curated[i]))
     assign(curatedDatObj[i], res)
 }
 
@@ -25,5 +25,5 @@ curatedAll <- c(curatedDatObj, curatedTrtObj) # names of all the curated tables
 curated_all <- get(curatedAll[1])
 for (i in 2:length(curatedAll)) { # assuming there are >= 2 tables to be combined
     dat <- get(curatedAll[i])
-    curated_all <- left_join(curated_all, dat, by = "curation_id")
+    curated_all <- dplyr::left_join(curated_all, dat, by = "curation_id")
 }
